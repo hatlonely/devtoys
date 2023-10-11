3ed413071bdf4bb2f0d6e59668e015033a74c723
hatlonely <hatlonely@gmail.com>
2023-10-12 03:03:37 +0800

    build(deps): update package-lock.json, package.json, package.json.md5 and Base64TextApp.svelte

--- frontend/src/Base64TextApp.svelte
+++ frontend/src/Base64TextApp.svelte
@@ -2,6 +2,7 @@
   import "beercss";
   import "material-dynamic-colors";
   import { Base64Decode, Base64Encode } from "../wailsjs/go/apps/Base64TextApp";
+  import copy from "copy-to-clipboard";
 
   let decodedText = "";
   let encodedText = "";
@@ -9,6 +10,9 @@
   let type_ = "std";
   let raw = false;
   let decodeError = "";
+
+  let pasted = false;
+  let copied = false;
 
   let operation_infos = [
     {
@@ -31,69 +35,113 @@
       value: "std",
     },
   ];
+
+  function calculate() {
+    if (operation === "encode") {
+      Base64Encode(decodedText, type_, raw).then((result) => {
+        encodedText = result;
+      });
+    } else {
+      Base64Decode(encodedText, type_, raw)
+        .then((result) => {
+          decodedText = result;
+          decodeError = "";
+        })
+        .catch((err) => {
+          decodeError = err;
+        });
+    }
+    copied = false;
+    pasted = false;
+  }
+
+  $: raw, calculate();
+  $: type_, calculate();
+  $: operation, calculate();
 </script>
 
 <main class="responsive">
   <h3>Base64文本编解码</h3>
 
-  <div class="field middle-align no-margin">
-    <nav>
-      <div class="s1"><b>操作</b></div>
-      {#each operation_infos as info}
-        <label class="radio">
-          <input
-            type="radio"
-            name={info.text}
-            value={info.value}
-            bind:group={operation}
-          />
-          <span>{info.text}</span>
+  <div class="grid border round center abs">
+    <div class="s3 field middle-align no-margin">
+      <nav class="absolute center">
+        {#each operation_infos as info}
+          <label class="radio">
+            <input
+              type="radio"
+              name={info.text}
+              value={info.value}
+              bind:group={operation}
+            />
+            <span>{info.text}</span>
+          </label>
+        {/each}
+      </nav>
+    </div>
+    <div class="s3 field middle-align no-margin">
+      <nav class="absolute center">
+        {#each type_infos as info}
+          <label class="radio">
+            <input
+              type="radio"
+              name={info.text}
+              value={info.value}
+              bind:group={type_}
+            />
+            <span>{info.text}</span>
+          </label>
+        {/each}
+      </nav>
+    </div>
+
+    <div class="s3 field middle-align no-margin">
+      <nav class="absolute center">
+        <div class="s1">RAW</div>
+        <label class="switch">
+          <input type="checkbox" bind:checked={raw} />
+          <span />
         </label>
-      {/each}
-    </nav>
-  </div>
+      </nav>
+    </div>
 
-  <div class="field middle-align no-margin">
-    <nav>
-      <div class="s1"><b>类型</b></div>
-      {#each type_infos as info}
-        <label class="radio">
-          <input
-            type="radio"
-            name={info.text}
-            value={info.value}
-            bind:group={type_}
-          />
-          <span>{info.text}</span>
-        </label>
-      {/each}
-    </nav>
+    <div class="s3 field middle-align no-margin">
+      <nav class="absolute center">
+        <!-- 粘贴按钮 -->
+        <button
+          class="border square round small"
+          on:click={(e) => {
+            navigator.clipboard.readText().then((text) => {
+              if (operation === "encode") {
+                decodedText = text;
+              } else {
+                encodedText = text;
+              }
+              pasted = true;
+            });
+          }}
+        >
+          <i>{pasted ? "done" : "content_paste"}</i>
+          <div class="tooltip">paste</div>
+        </button>
+        <!-- 复制按钮 -->
+        <button
+          class="border square round small"
+          on:click={(e) => {
+            if (operation === "encode") {
+              copy(encodedText);
+            } else {
+              copy(decodedText);
+            }
+            copied = true;
+          }}
+        >
+          <i>{copied ? "done" : "content_copy"}</i>
+          <div class="tooltip">copy</div>
+        </button>
+      </nav>
+    </div>
   </div>
-
-  <div class="field middle-align no-margin">
-    <nav>
-      <div class="s1"><b>RAW</b></div>
-      <label class="switch">
-        <input type="checkbox" bind:checked={raw} />
-        <span />
-      </label>
-    </nav>
-  </div>
-
-  <nav class="no-space">
-    <button class="border left-round">
-      <i class="small">check</i>
-      <span>Button</span>
-    </button>
-    <button class="border no-round">
-      <i class="small">check</i>
-      <span>Button</span>
-    </button>
-    <button class="border right-round fill">
-      <i class="small">check</i>
-      <span>Button</span>
-    </button>
-  </nav>
 
   <div
     class={"field textarea label border" +
@@ -103,13 +151,7 @@
       bind:value={decodedText}
       name="decodedTextArea"
       disabled={operation === "decode"}
-      on:change={(e) => {
-        if (operation === "encode") {
-          Base64Encode(decodedText, type_, raw).then((result) => {
-            encodedText = result;
-          });
-        }
-      }}
+      on:change={calculate}
     />
     <label for="decodedTextArea">编码前字符串</label>
   </div>
@@ -121,22 +163,11 @@
   >
     <textarea
       bind:value={encodedText}
-      name="encodeTextArea"
+      name="encodedTextArea"
       disabled={operation === "encode"}
-      on:change={(e) => {
-        if (operation === "decode") {
-          Base64Decode(encodedText, type_, raw)
-            .then((result) => {
-              decodedText = result;
-              decodeError = "";
-            })
-            .catch((err) => {
-              decodeError = err;
-            });
-        }
-      }}
+      on:change={calculate}
     />
-    <label for="encodeTextArea">解码后字符串</label>
+    <label for="encodedTextArea">解码后字符串</label>
     <span class="error">{decodeError}</span>
   </div>
 </main>
