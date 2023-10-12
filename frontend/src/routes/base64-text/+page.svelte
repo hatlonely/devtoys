9efa11ee713e1bfa0ebc1f78cb1324f275c88d15
hatlonely <hatlonely@gmail.com>
2023-10-13 01:56:22 +0800

    update

---
+++ frontend/src/routes/base64-text/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import 'material-icons/iconfont/material-icons.css';
+</script>
+
+<AppShell>
+	<svelte:fragment>
+		<div class="card p-4 w-full text-token space-y-4">
+			<div class="card p-4">
+				<span class="badge"><span class="material-icons-outlined">sync_alt</span></span>
+				<span class="">转换</span>
+				<div class="card p-4 flex justify-center items-center">
+					<span>Minimal</span>
+				</div>
+			</div>
+
+			<label class="label">
+				<textarea
+					class="textarea"
+					rows="4"
+					placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+				/>
+			</label>
+
+			<label class="label">
+				<textarea
+					class="textarea"
+					rows="4"
+					placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+				/>
+			</label>
+		</div>
+	</svelte:fragment>
+</AppShell>
