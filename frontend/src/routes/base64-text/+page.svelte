b523070370170bc700e7fe0c02e01ec73e16c856
hatlonely <hatlonely@gmail.com>
2023-10-14 07:39:42 +0800

    update package.json.md5, +page.svelte and vite.config.ts

--- frontend/src/routes/base64-text/+page.svelte
+++ frontend/src/routes/base64-text/+page.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
-	import { AppShell } from '@skeletonlabs/skeleton';
 	import 'material-symbols';
+	import { AppShell } from '@skeletonlabs/skeleton';
 	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import { Base64Decode, Base64Encode } from '../../../wailsjs/go/apps/Base64TextApp';
 
 	let op = 'encode';
 	let mode = 'std';
-	let align = 'true';
+	let align = true;
+	let input = '';
+	let output = '';
 
-	const op_desc_map: Map<string, string> = new Map([
-		['encode', '编码'],
-		['decode', '解码']
-	]);
+	function onInput(e) {
+		if (op === 'encode') {
+			Base64Encode(input, mode, align).then((res) => {
+				output = res;
+			});
+		} else {
+			Base64Decode(input, mode, align)
+				.then((res) => {
+					output = res;
+				})
+				.catch((err) => {
+					output = err;
+				});
+		}
+	}
 </script>
 
 <AppShell>
@@ -19,7 +34,7 @@
 			<h2 class="h2 my-4">Base64 文本编解/解码工具</h2>
 
 			<div class="w-full text-token card p-4 space-y-4">
-				<div class="w-full text-token p-2 md:flex">
+				<div class="w-full text-token p-2 flex flex-row">
 					<span class="badge-icon p-4 variant-soft-secondary mt-2">
 						<span class="material-symbols-outlined">sync_alt</span>
 					</span>
@@ -35,7 +50,7 @@
 					</span>
 				</div>
 
-				<div class="w-full text-token p-2 md:flex">
+				<div class="w-full text-token p-2 flex flex-row">
 					<span class="badge-icon p-4 variant-soft-secondary mt-2">
 						<span class="material-symbols-outlined">link</span>
 					</span>
@@ -51,7 +66,7 @@
 					</span>
 				</div>
 
-				<div class="w-full text-token p-2 md:flex">
+				<div class="w-full text-token p-2 flex flex-row">
 					<span class="badge-icon p-4 variant-soft-secondary mt-2">
 						<span class="material-symbols-outlined">equal</span>
 					</span>
@@ -61,28 +76,30 @@
 					</span>
 					<span>
 						<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
-							<RadioItem bind:group={align} name="align" value="true">开启</RadioItem>
-							<RadioItem bind:group={align} name="align" value="false">关闭</RadioItem>
+							<RadioItem bind:group={align} name="align" value={true}>开启</RadioItem>
+							<RadioItem bind:group={align} name="align" value={false}>关闭</RadioItem>
 						</RadioGroup>
 					</span>
 				</div>
 			</div>
 
-			<label class="label">
-				<textarea
-					class="textarea"
-					rows="4"
-					placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-				/>
-			</label>
+			<div class="flex flex-row-reverse">
+				<button type="button" class="btn btn-sm variant-filled mx-2">
+					<span>清空</span> <span class="material-symbols-outlined">delete</span>
+				</button>
+				<button type="button" class="btn btn-sm variant-filled mx-2">
+					<span>粘贴</span> <span class="material-symbols-outlined">content_paste</span>
+				</button>
+			</div>
+			<textarea
+				bind:value={input}
+				on:input={onInput}
+				class="textarea"
+				rows="4"
+				placeholder="输入要编码/解码的文本"
+			/>
 
-			<label class="label">
-				<textarea
-					class="textarea"
-					rows="4"
-					placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-				/>
-			</label>
+			<CodeBlock bind:code={output} />
 		</div>
 	</svelte:fragment>
 </AppShell>
