7fe1b070ae02b73ddc5239298633786a487c277f
hatlonely <hatlonely@gmail.com>
2023-10-14 23:07:18 +0800

    update +page.svelte and +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,7 @@
-<div class="container mx-auto p-8 space-y-8">
-	<h1 class="h1">Hello Skeleton</h1>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-	<section>
-		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
-		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
-		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
-	</section>
-</div>
-
 <script>
 	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<Avatar src="https://i.pravatar.cc/" />
+<div class="w-full text-token px-6 py-3 space-y-4">
+	<h2 class="h2 my-4">所有工具</h2>
+</div>
--- frontend/src/routes/base64-text/+page.svelte
+++ frontend/src/routes/base64-text/+page.svelte
@@ -67,138 +67,134 @@
 	$: align, calculate();
 </script>
 
-<AppShell>
-	<svelte:fragment>
-		<div class="p-4 w-full text-token space-y-4 mt-2">
-			<h2 class="h2 my-4">Base64 文本编解/解码工具</h2>
+<div class="w-full text-token px-6 py-3 space-y-4">
+	<h2 class="h2 my-4">Base64 文本编解/解码工具</h2>
 
-			<div class="w-full text-token card p-4 space-y-4">
-				<div class="w-full text-token p-2 flex flex-row">
-					<span class="badge-icon p-4 variant-soft-secondary mt-2">
-						<span class="material-symbols-outlined">sync_alt</span>
-					</span>
-					<span class="flex-auto ml-5">
-						<dt class="font-bold">转换模式</dt>
-						<dd class="text-sm opacity-50">选择您想使用的转换模式</dd>
-					</span>
-					<span>
-						<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
-							<RadioItem bind:group={op} name="op" value="encode">编码</RadioItem>
-							<RadioItem bind:group={op} name="op" value="decode">解码</RadioItem>
-						</RadioGroup>
-					</span>
-				</div>
-
-				<div class="w-full text-token p-2 flex flex-row">
-					<span class="badge-icon p-4 variant-soft-secondary mt-2">
-						<span class="material-symbols-outlined">link</span>
-					</span>
-					<span class="flex-auto ml-5">
-						<dt class="font-bold">编码模式</dt>
-						<dd class="text-sm opacity-50">常规编码/URL安全编码</dd>
-					</span>
-					<span>
-						<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
-							<RadioItem bind:group={mode} name="mode" value="std">常规</RadioItem>
-							<RadioItem bind:group={mode} name="mode" value="url">URL</RadioItem>
-						</RadioGroup>
-					</span>
-				</div>
-
-				<div class="w-full text-token p-2 flex flex-row">
-					<span class="badge-icon p-4 variant-soft-secondary mt-2">
-						<span class="material-symbols-outlined">equal</span>
-					</span>
-					<span class="flex-auto ml-5">
-						<dt class="font-bold">补齐模式</dt>
-						<dd class="text-sm opacity-50">末尾是否使用 = 补齐</dd>
-					</span>
-					<span>
-						<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
-							<RadioItem bind:group={align} name="align" value={true}>开启</RadioItem>
-							<RadioItem bind:group={align} name="align" value={false}>关闭</RadioItem>
-						</RadioGroup>
-					</span>
-				</div>
-			</div>
-
-			<div class="flex flex-row-reverse">
-				<button
-					on:click={clear}
-					on:click={() => {
-						cleared = true;
-						setTimeout(() => {
-							cleared = false;
-						}, 2000);
-					}}
-					type="button"
-					class="btn btn-sm variant-filled mx-2"
-				>
-					<span>清空</span>
-					<span class="material-symbols-outlined">
-						{cleared ? 'done' : 'delete'}
-					</span>
-				</button>
-				<button
-					on:click={paste}
-					on:click={() => {
-						pasted = true;
-						setTimeout(() => {
-							pasted = false;
-						}, 2000);
-					}}
-					type="button"
-					class="btn btn-sm variant-filled mx-2"
-				>
-					<span>粘贴</span>
-					<span class="material-symbols-outlined">
-						{pasted ? 'done' : 'content_paste'}
-					</span>
-				</button>
-			</div>
-
-			<textarea
-				bind:value={input}
-				on:input={calculate}
-				on:input={updateTextareaHeight}
-				class="textarea app-code"
-				rows="4"
-				placeholder="输入要编码/解码的文本"
-			/>
-
-			{#if warning}
-				<div class="alert variant-filled-error app-code">
-					<span class="material-symbols-outlined">warning</span>
-					<div class="alert-message" data-toc-ignore>
-						<h3 class="h3" data-toc-ignore>Warning</h3>
-						<p>{warning}</p>
-					</div>
-				</div>
-			{/if}
-
-			{#if output}
-				<div class="w-full card p-4">
-					<button
-						type="button"
-						class="btn btn-sm p-0 float-right"
-						on:click={() => {
-							copied = true;
-							setTimeout(() => {
-								copied = false;
-							}, 2000);
-						}}
-						use:clipboard={output}
-					>
-						<span class="material-symbols-outlined">
-							{copied ? 'done' : 'content_copy'}
-						</span>
-					</button>
-					<pre class="app-code">{output}</pre>
-				</div>
-			{/if}
+	<div class="w-full text-token card p-4 space-y-4">
+		<div class="w-full text-token p-2 flex flex-row">
+			<span class="badge-icon p-4 variant-soft-secondary mt-2">
+				<span class="material-symbols-outlined">sync_alt</span>
+			</span>
+			<span class="flex-auto ml-5">
+				<dt class="font-bold">转换模式</dt>
+				<dd class="text-sm opacity-50">选择您想使用的转换模式</dd>
+			</span>
+			<span>
+				<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
+					<RadioItem bind:group={op} name="op" value="encode">编码</RadioItem>
+					<RadioItem bind:group={op} name="op" value="decode">解码</RadioItem>
+				</RadioGroup>
+			</span>
 		</div>
-	</svelte:fragment>
-</AppShell>
+
+		<div class="w-full text-token p-2 flex flex-row">
+			<span class="badge-icon p-4 variant-soft-secondary mt-2">
+				<span class="material-symbols-outlined">link</span>
+			</span>
+			<span class="flex-auto ml-5">
+				<dt class="font-bold">编码模式</dt>
+				<dd class="text-sm opacity-50">常规编码/URL安全编码</dd>
+			</span>
+			<span>
+				<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
+					<RadioItem bind:group={mode} name="mode" value="std">常规</RadioItem>
+					<RadioItem bind:group={mode} name="mode" value="url">URL</RadioItem>
+				</RadioGroup>
+			</span>
+		</div>
+
+		<div class="w-full text-token p-2 flex flex-row">
+			<span class="badge-icon p-4 variant-soft-secondary mt-2">
+				<span class="material-symbols-outlined">equal</span>
+			</span>
+			<span class="flex-auto ml-5">
+				<dt class="font-bold">补齐模式</dt>
+				<dd class="text-sm opacity-50">末尾是否使用 = 补齐</dd>
+			</span>
+			<span>
+				<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
+					<RadioItem bind:group={align} name="align" value={true}>开启</RadioItem>
+					<RadioItem bind:group={align} name="align" value={false}>关闭</RadioItem>
+				</RadioGroup>
+			</span>
+		</div>
+	</div>
+
+	<div class="flex flex-row-reverse">
+		<button
+			on:click={clear}
+			on:click={() => {
+				cleared = true;
+				setTimeout(() => {
+					cleared = false;
+				}, 2000);
+			}}
+			type="button"
+			class="btn btn-sm variant-filled mx-2"
+		>
+			<span>清空</span>
+			<span class="material-symbols-outlined">
+				{cleared ? 'done' : 'delete'}
+			</span>
+		</button>
+		<button
+			on:click={paste}
+			on:click={() => {
+				pasted = true;
+				setTimeout(() => {
+					pasted = false;
+				}, 2000);
+			}}
+			type="button"
+			class="btn btn-sm variant-filled mx-2"
+		>
+			<span>粘贴</span>
+			<span class="material-symbols-outlined">
+				{pasted ? 'done' : 'content_paste'}
+			</span>
+		</button>
+	</div>
+
+	<textarea
+		bind:value={input}
+		on:input={calculate}
+		on:input={updateTextareaHeight}
+		class="textarea app-code"
+		rows="4"
+		placeholder="输入要编码/解码的文本"
+	/>
+
+	{#if warning}
+		<div class="alert variant-filled-error app-code">
+			<span class="material-symbols-outlined">warning</span>
+			<div class="alert-message" data-toc-ignore>
+				<h3 class="h3" data-toc-ignore>Warning</h3>
+				<p>{warning}</p>
+			</div>
+		</div>
+	{/if}
+
+	{#if output}
+		<div class="w-full card p-4">
+			<button
+				type="button"
+				class="btn btn-sm p-0 float-right"
+				on:click={() => {
+					copied = true;
+					setTimeout(() => {
+						copied = false;
+					}, 2000);
+				}}
+				use:clipboard={output}
+			>
+				<span class="material-symbols-outlined">
+					{copied ? 'done' : 'content_copy'}
+				</span>
+			</button>
+			<pre class="app-code">{output}</pre>
+		</div>
+	{/if}
+</div>
 
 <style>
 	.app-code {
