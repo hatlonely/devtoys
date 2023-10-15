97b16627242ab4693d9f0dbc5326a2ae52db7d92
hatlonely <hatlonely@gmail.com>
2023-10-16 03:09:33 +0800

    update InformationWall.svelte

--- frontend/src/lib/components/InformationWall.svelte
+++ frontend/src/lib/components/InformationWall.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex flex-wrap space-y-1">
 	{#each labelValues as labelValue, i}
-		<div class="w-1/3">
+		<div class="sm:w-full md:w-full lg:w-1/2 xl:w-1/3 2xl:w-1/4">
 			<div class="flex flex-row space-x-3">
 				<div class="align-sub">
 					<span class="text-gray-500 align-sub">{labelValue.label}</span>
