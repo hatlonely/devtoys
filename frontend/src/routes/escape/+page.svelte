e851f82a76372e1f6e1e0f721f72986e6e289753
hatlonely <hatlonely@gmail.com>
2023-10-16 03:01:51 +0800

    update +page.svelte

--- frontend/src/routes/escape/+page.svelte
+++ frontend/src/routes/escape/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	import 'material-symbols';
-	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
 	import { Escape } from '$lib/wailsjs/go/devtoys/App';
-	import { clipboard } from '@skeletonlabs/skeleton';
+	import { Title, RadioGroup as RadioGroup2, Textarea, TextViewer } from '$lib';
 	import '@fontsource/roboto-mono';
 
 	let mode = 'unescape';
@@ -10,9 +8,6 @@
 	let input = '';
 	let output = '';
 	let warning = '';
-	let copied = false;
-	let pasted = false;
-	let cleared = false;
 
 	function calculate() {
 		Escape({
@@ -29,29 +24,6 @@
 			});
 	}
 
-	function paste() {
-		navigator.clipboard.readText().then((text) => {
-			input = text;
-			calculate();
-		});
-	}
-
-	function clear() {
-		input = '';
-		output = '';
-	}
-
-	function updateTextareaHeight(event: Event) {
-		const target = event.target as HTMLTextAreaElement;
-		target.style.height = 'auto';
-
-		if (target.scrollHeight > window.innerHeight / 3) {
-			target.style.height = `${window.innerHeight / 3}px`;
-		} else {
-			target.style.height = `${target.scrollHeight + 1}px`;
-		}
-	}
-
 	function onModeChange() {
 		input = output;
 		calculate();
@@ -62,133 +34,67 @@
 </script>
 
 <div class="w-full text-token px-6 py-3 space-y-4">
-	<h2 class="h2 my-4">转义/反转义工具</h2>
+	<Title title="转义/反转义工具" />
 
-	<div class="w-full text-token card p-4 space-y-4">
-		<div class="w-full text-token p-2 flex flex-row">
-			<span class="badge-icon p-4 variant-soft-secondary mt-2">
-				<span class="material-symbols-outlined">sync_alt</span>
-			</span>
-			<span class="flex-auto ml-5">
-				<dt class="font-bold">转义模式</dt>
-				<dd class="text-sm opacity-50">选择您想使用的转义模式</dd>
-			</span>
-			<span>
-				<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
-					<RadioItem bind:group={mode} name="mode" value="escape">转义</RadioItem>
-					<RadioItem bind:group={mode} name="mode" value="unescape">反转义</RadioItem>
-				</RadioGroup>
-			</span>
-		</div>
+	<div class="w-full text-token card p-4">
+		<RadioGroup2
+			bind:group={mode}
+			name="mode"
+			title="转义模式"
+			description="选择您想使用的转义模式"
+			icon="sync_alt"
+			items={[
+				{
+					label: '转义',
+					value: 'escape'
+				},
+				{
+					label: '反转义',
+					value: 'unescape'
+				}
+			]}
+		/>
 
-		<div class="w-full text-token p-2 flex flex-row">
-			<span class="badge-icon p-4 variant-soft-secondary mt-2">
-				<span class="material-symbols-outlined">link</span>
-			</span>
-			<span class="flex-auto ml-5">
-				<dt class="font-bold">转义类型</dt>
-				<dd class="text-sm opacity-50">常规编码/URL安全编码</dd>
-			</span>
-			<span>
-				<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
-					<RadioItem bind:group={type_} name="type" value="html">HTML</RadioItem>
-					<RadioItem bind:group={type_} name="type" value="url">URL</RadioItem>
-					<RadioItem bind:group={type_} name="type" value="urlquery">URLQuery</RadioItem>
-					<RadioItem bind:group={type_} name="type" value="string">String</RadioItem>
-				</RadioGroup>
-			</span>
-		</div>
+		<RadioGroup2
+			bind:group={type_}
+			name="type_"
+			title="转义类型"
+			description="常规编码/URL安全编码"
+			icon="link"
+			items={[
+				{
+					label: 'HTML',
+					value: 'html'
+				},
+				{
+					label: 'URL',
+					value: 'url'
+				},
+				{
+					label: 'URLQuery',
+					value: 'urlquery'
+				},
+				{
+					label: 'String',
+					value: 'string'
+				}
+			]}
+		/>
 	</div>
 
-	<div class="flex flex-row-reverse">
-		<button
-			on:click={clear}
-			on:click={() => {
-				cleared = true;
-				setTimeout(() => {
-					cleared = false;
-				}, 2000);
-			}}
-			type="button"
-			class="btn btn-sm variant-filled-primary mx-2"
-		>
-			<span>清空</span>
-			<span class="material-symbols-outlined">
-				{cleared ? 'done' : 'delete'}
-			</span>
-		</button>
-		<button
-			on:click={paste}
-			on:click={() => {
-				pasted = true;
-				setTimeout(() => {
-					pasted = false;
-				}, 2000);
-			}}
-			type="button"
-			class="btn btn-sm variant-filled-primary mx-2"
-		>
-			<span>粘贴</span>
-			<span class="material-symbols-outlined">
-				{pasted ? 'done' : 'content_paste'}
-			</span>
-		</button>
+	<div class="w-full text-token card p-4">
+		<Textarea
+			bind:value={input}
+			on:input={calculate}
+			placeholder="输入要编码/解码的文本"
+			code={true}
+			{warning}
+		/>
 	</div>
-
-	<textarea
-		bind:value={input}
-		on:input={calculate}
-		on:input={updateTextareaHeight}
-		class="textarea app-code"
-		rows="4"
-		placeholder="输入要编码/解码的文本"
-	/>
-
-	{#if warning}
-		<div class="alert variant-filled-error app-code">
-			<span class="material-symbols-outlined">warning</span>
-			<div class="alert-message" data-toc-ignore>
-				<h3 class="h3" data-toc-ignore>Warning</h3>
-				<p>{warning}</p>
-			</div>
-		</div>
-	{/if}
 
 	{#if output}
-		<div class="w-full card p-4">
-			<button
-				type="button"
-				class="btn btn-sm p-0 float-right variant-filled-primary"
-				on:click={() => {
-					copied = true;
-					setTimeout(() => {
-						copied = false;
-					}, 2000);
-				}}
-				use:clipboard={output}
-			>
-				<span class="material-symbols-outlined">
-					{copied ? 'done' : 'content_copy'}
-				</span>
-			</button>
-			<pre class="app-code">{output}</pre>
+		<div class="w-full text-token card p-4">
+			<TextViewer bind:text={output} />
 		</div>
 	{/if}
 </div>
-
-<style>
-	.app-code {
-		font-size: 0.8rem;
-		font-family: 'Roboto Mono', monospace;
-		overflow-wrap: anywhere;
-		white-space: pre-wrap; /* Since CSS 2.1 */
-		white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
-		white-space: -pre-wrap; /* Opera 4-6 */
-		white-space: -o-pre-wrap; /* Opera 7 */
-		word-wrap: break-word; /* Internet Explorer 5.5+ */
-	}
-
-	.material-symbols-outlined {
-		font-variation-settings: 'FILL' 0, 'wght' 200, 'GRAD' 0, 'opsz' 24;
-	}
-</style>
