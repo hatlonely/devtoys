f2693e0975a24655394adfb236a0997a819b6411
hatlonely <hatlonely@gmail.com>
2023-10-17 23:39:14 +0800

    update hash_app.go and hash_app_test.go

--- internal/devtoys/hash_app.go
+++ internal/devtoys/hash_app.go
@@ -2,10 +2,12 @@ package devtoys
 
 import (
 	"context"
+	"crypto/hmac"
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
+	"encoding/base64"
 	"encoding/hex"
 )
 
@@ -22,8 +24,11 @@ func (a *HashApp) Startup(ctx context.Context) {
 }
 
 type HashReq struct {
-	Text string
-	Type string `validate:"required,oneof=md5 sha1 sha256 sha224 sha512"`
+	Text   string
+	Type   string `validate:"required,oneof=md5 sha1 sha256 sha224 sha512"`
+	Hmac   bool
+	Key    string
+	Encode string `validate:"oneof=hex base64"`
 }
 
 type HashRes struct {
@@ -31,45 +36,98 @@ type HashRes struct {
 }
 
 func (a *HashApp) Hash(req *HashReq) (*HashRes, error) {
-	res := &HashRes{}
-	switch req.Type {
-	case "md5":
-		res.Text = md5sum(req.Text)
-	case "sha1":
-		res.Text = sha1sum(req.Text)
-	case "sha256":
-		res.Text = sha256sum(req.Text)
-	case "sha224":
-		res.Text = sha224sum(req.Text)
-	case "sha512":
-		res.Text = sha512sum(req.Text)
-	default:
-		res.Text = "unknown hash type"
+	var buf []byte
+	if req.Hmac {
+		switch req.Type {
+		case "md5":
+			buf = hmacMd5([]byte(req.Key), []byte(req.Text))
+		case "sha1":
+			buf = hmacSha1([]byte(req.Key), []byte(req.Text))
+		case "sha256":
+			buf = hmacSha256([]byte(req.Key), []byte(req.Text))
+		case "sha224":
+			buf = hmacSha224([]byte(req.Key), []byte(req.Text))
+		case "sha512":
+			buf = hmacSha512([]byte(req.Key), []byte(req.Text))
+		}
+	} else {
+		switch req.Type {
+		case "md5":
+			buf = md5sum([]byte(req.Text))
+		case "sha1":
+			buf = sha1sum([]byte(req.Text))
+		case "sha256":
+			buf = sha256sum([]byte(req.Text))
+		case "sha224":
+			buf = sha224sum([]byte(req.Text))
+		case "sha512":
+			buf = sha512sum([]byte(req.Text))
+		}
 	}
-	return res, nil
+
+	var text string
+	if req.Encode == "hex" {
+		text = hex.EncodeToString(buf)
+	} else {
+		text = base64.StdEncoding.EncodeToString(buf)
+	}
+
+	return &HashRes{
+		Text: text,
+	}, nil
 }
 
-func md5sum(text string) string {
+func md5sum(text []byte) []byte {
 	md5 := md5.Sum([]byte(text))
-	return hex.EncodeToString(md5[:])
+	return md5[:]
 }
 
-func sha1sum(text string) string {
+func sha1sum(text []byte) []byte {
 	sha1 := sha1.Sum([]byte(text))
-	return hex.EncodeToString(sha1[:])
+	return sha1[:]
 }
 
-func sha256sum(text string) string {
+func sha256sum(text []byte) []byte {
 	sha256 := sha256.Sum256([]byte(text))
-	return hex.EncodeToString(sha256[:])
+	return sha256[:]
 }
 
-func sha224sum(text string) string {
+func sha224sum(text []byte) []byte {
 	sha224 := sha256.Sum224([]byte(text))
-	return hex.EncodeToString(sha224[:])
+	return sha224[:]
 }
 
-func sha512sum(text string) string {
+func sha512sum(text []byte) []byte {
 	sha512 := sha512.Sum512([]byte(text))
-	return hex.EncodeToString(sha512[:])
+	return sha512[:]
+}
+
+func hmacMd5(key []byte, text []byte) []byte {
+	hmac := hmac.New(md5.New, []byte(key))
+	hmac.Write([]byte(text))
+	return hmac.Sum(nil)
+}
+
+func hmacSha1(key []byte, text []byte) []byte {
+	hmac := hmac.New(sha1.New, []byte(key))
+	hmac.Write([]byte(text))
+	return hmac.Sum(nil)
+}
+
+func hmacSha256(key []byte, text []byte) []byte {
+	hmac := hmac.New(sha256.New, []byte(key))
+	hmac.Write([]byte(text))
+	return hmac.Sum(nil)
+}
+
+func hmacSha224(key []byte, text []byte) []byte {
+	hmac := hmac.New(sha256.New224, []byte(key))
+	hmac.Write([]byte(text))
+	return hmac.Sum(nil)
+}
+
+func hmacSha512(key []byte, text []byte) []byte {
+	hmac := hmac.New(sha512.New, []byte(key))
+	hmac.Write([]byte(text))
+	return hmac.Sum(nil)
 }
--- internal/devtoys/hash_app_test.go
+++ internal/devtoys/hash_app_test.go
@@ -1,6 +1,7 @@
 package devtoys
 
 import (
+	"encoding/hex"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -8,30 +9,70 @@ import (
 
 func TestMd5(t *testing.T) {
 	Convey("TestMd5", t, func() {
-		So(md5sum("hello world"), ShouldEqual, "5eb63bbbe01eeed093cb22bb8f5acdc3")
+		buf := md5sum([]byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "5eb63bbbe01eeed093cb22bb8f5acdc3")
 	})
 }
 
 func TestSha1(t *testing.T) {
 	Convey("TestSha1", t, func() {
-		So(sha1sum("hello world"), ShouldEqual, "2aae6c35c94fcfb415dbe95f408b9ce91ee846ed")
+		buf := sha1sum([]byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "2aae6c35c94fcfb415dbe95f408b9ce91ee846ed")
 	})
 }
 
 func TestSha256(t *testing.T) {
 	Convey("TestSha256", t, func() {
-		So(sha256sum("hello world"), ShouldEqual, "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9")
+		buf := sha256sum([]byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9")
 	})
 }
 
 func TestSha224(t *testing.T) {
 	Convey("TestSha224", t, func() {
-		So(sha224sum("hello world"), ShouldEqual, "2f05477fc24bb4faefd86517156dafdecec45b8ad3cf2522a563582b")
+		buf := sha224sum([]byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "2f05477fc24bb4faefd86517156dafdecec45b8ad3cf2522a563582b")
 	})
 }
 
 func TestSha512(t *testing.T) {
 	Convey("TestSha512", t, func() {
-		So(sha512sum("hello world"), ShouldEqual, "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f")
+		buf := sha512sum([]byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f")
+	})
+}
+
+func TestHmacMd5(t *testing.T) {
+	Convey("TestHmacMd5", t, func() {
+		buf := hmacMd5([]byte("123456"), []byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "9028905855a81e0f3f76a72212e6c488")
+	})
+}
+
+func TestHmacSha1(t *testing.T) {
+	Convey("TestHmacSha1", t, func() {
+		buf := hmacSha1([]byte("123456"), []byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "4873e80b320876fef98bd5857a7146db412007c7")
+	})
+}
+
+func TestHmacSha256(t *testing.T) {
+	Convey("TestHmacSha256", t, func() {
+		buf := hmacSha256([]byte("123456"), []byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "83b3eb2788457b46a2f17aaa048f795af0d9dabb8e5924dd2fc0ea682d929fe5")
+	})
+}
+
+func TestHmacSha224(t *testing.T) {
+	Convey("TestHmacSha224", t, func() {
+		buf := hmacSha224([]byte("123456"), []byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "4f358a44c7bfaf92a2aa3e015d06a6913cc22dd1d684efbb3ce71755")
+	})
+}
+
+func TestHmacSha512(t *testing.T) {
+	Convey("TestHmacSha512", t, func() {
+		buf := hmacSha512([]byte("123456"), []byte("hello world"))
+		So(hex.EncodeToString(buf), ShouldEqual, "6c9c251365f3507dc923023fd8e180925eee0dc0bb467d156edc21b9889fc1115cbd7a948090abb59b31718e83978900d7582993392d90d2835ee13c9f2fbb69")
 	})
 }
