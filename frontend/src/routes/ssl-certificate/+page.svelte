010c83ed77120d97b49accf020a3de1bb880ef34
hatlonely <hatlonely@gmail.com>
2023-10-21 18:11:39 +0800

    update +page.svelte

--- frontend/src/routes/ssl-certificate/+page.svelte
+++ frontend/src/routes/ssl-certificate/+page.svelte
@@ -42,15 +42,15 @@
 			rows: [
 				{
 					label: '国家',
-					value: res.Subject.Country ? res.Subject.Country.join(', ') : ''
+					value: res?.Subject?.Country?.join(', ')
 				},
 				{
 					label: '组织',
-					value: res.Subject.Organization ? res.Subject.Organization.join(', ') : ''
+					value: res?.Subject?.Organization?.join(', ')
 				},
 				{
 					label: '组织单位',
-					value: res.Subject.OrganizationUnit ? res.Subject.OrganizationUnit.join(', ') : ''
+					value: res?.Subject?.OrganizationUnit?.join(', ')
 				},
 				{
 					label: '域名',
@@ -58,19 +58,19 @@
 				},
 				{
 					label: '省份',
-					value: res.Subject.Province ? res.Subject.Province.join(', ') : ''
+					value: res?.Subject?.Province?.join(', ')
 				},
 				{
 					label: '地区',
-					value: res.Subject.Locality ? res.Subject.Locality.join(', ') : ''
+					value: res?.Subject?.Locality?.join(', ')
 				},
 				{
 					label: '街道地址',
-					value: res.Subject.StreetAddress ? res.Subject.StreetAddress.join(', ') : ''
+					value: res?.Subject?.StreetAddress?.join(', ')
 				},
 				{
 					label: '邮政编码',
-					value: res.Subject.PostalCode ? res.Subject.PostalCode.join(', ') : ''
+					value: res?.Subject?.PostalCode?.join(', ')
 				}
 			]
 		};
@@ -83,15 +83,15 @@
 			rows: [
 				{
 					label: '国家',
-					value: res.Issuer.Country ? res.Issuer.Country.join(', ') : ''
+					value: res?.Issuer?.Country?.join(', ')
 				},
 				{
 					label: '组织',
-					value: res.Issuer.Organization ? res.Issuer.Organization.join(', ') : ''
+					value: res?.Issuer?.Organization?.join(', ')
 				},
 				{
 					label: '组织单位',
-					value: res.Issuer.OrganizationUnit ? res.Issuer.OrganizationUnit.join(', ') : ''
+					value: res?.Issuer?.OrganizationUnit?.join(', ')
 				},
 				{
 					label: '颁发机构',
@@ -127,23 +127,23 @@
 				{ label: 'SHA256', value: res.Certificate.Sha256 },
 				{
 					label: 'DNS 域名',
-					value: res.Certificate.DNSNames ? res.Certificate.DNSNames.join(', ') : ''
+					value: res?.Certificate?.DNSNames?.join(', ')
 				},
 				{
 					label: 'CRL 分发点',
 					value: res.Certificate.CRLDistributionPoints
-						? res.Certificate.CRLDistributionPoints.join(', ')
+						? res?.Certificate?.CRLDistributionPoints?.join(', ')
 						: ''
 				},
 				{
 					label: '颁发者证书 URL',
 					value: res.Certificate.IssuingCertificateURL
-						? res.Certificate.IssuingCertificateURL.join(', ')
+						? res?.Certificate?.IssuingCertificateURL?.join(', ')
 						: ''
 				},
 				{
 					label: 'OCSP 服务器地址',
-					value: res.Certificate.OCSPServer ? res.Certificate.OCSPServer.join(', ') : ''
+					value: res?.Certificate?.OCSPServer?.join(', ')
 				}
 			]
 		};
