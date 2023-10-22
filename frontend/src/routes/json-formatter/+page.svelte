3ad859c8e211ed7d8844ff221b3cedfbf4ac5f95
hatlonely <hatlonely@gmail.com>
2023-10-23 00:37:28 +0800

    update +page.svelte

--- frontend/src/routes/json-formatter/+page.svelte
+++ frontend/src/routes/json-formatter/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { CodeEditor, CodeViewer } from '$lib';
+	import { CodeEditor, CodeViewer, RadioGroupItem, NumberInputItem } from '$lib';
 
 	let text = '';
 	let json = '';
+	let indent = 2;
 	let warning: any = '';
 
 	async function calculate() {
@@ -14,16 +15,26 @@
 		warning = '';
 		try {
 			const obj = JSON.parse(text);
-			json = JSON.stringify(obj, null, 2);
+			json = JSON.stringify(obj, null, indent);
 		} catch (err) {
 			warning = err;
 		}
 	}
 
 	$: text, calculate();
+	$: indent, calculate();
 </script>
 
 <div class="w-full text-token px-6 py-3 space-y-4">
+	<div class="w-full text-token card p-4">
+		<NumberInputItem
+			bind:value={indent}
+			title="缩进空格"
+			description="缩进空格数量"
+			icon="sync_alt"
+		/>
+	</div>
+
 	<div class="w-full text-token card p-4">
 		<CodeEditor title="输入" bind:value={text} on:input={calculate} {warning} />
 	</div>
