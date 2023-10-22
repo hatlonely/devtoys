e7d8da5ed3f981da1f7176d77c0d78759166d8d2
hatlonely <hatlonely@gmail.com>
2023-10-22 17:20:58 +0800

    update .gitignore, text_binary_conversion_app.go and text_binary_conversion_app_test.go

--- internal/devtoys/text_binary_conversion_app.go
+++ internal/devtoys/text_binary_conversion_app.go
@@ -19,20 +19,27 @@ func NewTextBinaryConversionApp() *TextBinaryConversionApp {
 }
 
 type TextBinaryConversionReq struct {
-	Text            string
-	To              string `validate:"required,oneof=bin hex dec base64"`
+	In              string
+	InType          string `validate:"required,oneof=bin hex dec raw"`
+	ToType          string `validate:"required,oneof=bin hex dec raw"`
+	AsByte          bool
 	LowerCase       bool
-	WithoutSpace    bool
 	WithoutFillZero bool
 }
 
 type TextBinaryConversionRes struct {
-	Text string
+	To string
 }
 
 func (a *TextBinaryConversionApp) ConvertTextBinary(req *TextBinaryConversionReq) (*TextBinaryConversionRes, error) {
-	if req.Text == "" {
+	if req.In == "" {
 		return &TextBinaryConversionRes{}, nil
+	}
+	if req.InType == "" {
+		req.InType = "raw"
+	}
+	if req.ToType == "" {
+		req.ToType = "raw"
 	}
 
 	if err := validate.Struct(req); err != nil {
@@ -40,18 +47,19 @@ func (a *TextBinaryConversionApp) ConvertTextBinary(req *TextBinaryConversionReq
 	}
 
 	var text string
-	switch req.To {
-	case "bin":
-		text = strToBin(req.Text)
-	case "hex":
-		text = strToHex(req.Text)
-	case "dec":
-		text = strToDec(req.Text)
-	case "base64":
-		text = strToBase64(req.Text)
+	var err error
+
+	if req.AsByte {
+		text, err = convertByte(req.In, req.InType, req.ToType)
+	} else {
+		text, err = convertText(req.In, req.InType, req.ToType)
 	}
 
-	if !req.LowerCase && req.To == "hex" {
+	if err != nil {
+		return nil, errors.Wrap(err, "convertToText failed")
+	}
+
+	if !req.LowerCase && req.ToType == "hex" {
 		text = strings.ToUpper(text)
 	}
 
@@ -61,19 +69,109 @@ func (a *TextBinaryConversionApp) ConvertTextBinary(req *TextBinaryConversionReq
 		}).([]string), " ")
 	}
 
-	if req.WithoutSpace {
-		text = strings.ReplaceAll(text, " ", "")
-	}
-
 	return &TextBinaryConversionRes{
-		Text: text,
+		To: text,
 	}, nil
 }
 
-func strToBin(str string) string {
+func convertText(in string, inType string, toType string) (string, error) {
+	var text string
+	var err error
+
+	text, err = convertToText(in, inType)
+	if err != nil {
+		return "", errors.WithMessage(err, "convertToText failed")
+	}
+
+	text, err = convertFromText(text, toType)
+	if err != nil {
+		return "", errors.WithMessage(err, "convertFromText failed")
+	}
+
+	return text, nil
+}
+
+func convertByte(in string, inType string, toType string) (string, error) {
+	var buf []byte
+	var err error
+
+	buf, err = convertToByte(in, inType)
+	if err != nil {
+		return "", errors.WithMessage(err, "convertFromByte failed")
+	}
+
+	text, err := convertFromByte(buf, toType)
+	if err != nil {
+		return "", errors.WithMessage(err, "convertFromByte failed")
+	}
+
+	return text, nil
+}
+
+func convertToText(in string, inType string) (string, error) {
+	switch inType {
+	case "bin":
+		return binToText(in), nil
+	case "hex":
+		return hexToText(in), nil
+	case "dec":
+		return decToText(in), nil
+	case "raw":
+		return in, nil
+	}
+
+	return "", errors.New("invalid inType")
+}
+
+func convertToByte(in string, inType string) ([]byte, error) {
+	switch inType {
+	case "bin":
+		return binToByte(in), nil
+	case "hex":
+		return hexToByte(in), nil
+	case "dec":
+		return decToByte(in), nil
+	case "raw":
+		return base64.StdEncoding.DecodeString(in)
+	}
+
+	return nil, errors.New("invalid inType")
+}
+
+func convertFromText(in string, toType string) (string, error) {
+	switch toType {
+	case "bin":
+		return textToBin(in), nil
+	case "hex":
+		return textToHex(in), nil
+	case "dec":
+		return textToDec(in), nil
+	case "raw":
+		return in, nil
+	}
+
+	return "", errors.New("invalid toType")
+}
+
+func convertFromByte(in []byte, toType string) (string, error) {
+	switch toType {
+	case "bin":
+		return byteToBin(in), nil
+	case "hex":
+		return byteToHex(in), nil
+	case "dec":
+		return byteToDec(in), nil
+	case "raw":
+		return base64.StdEncoding.EncodeToString(in), nil
+	}
+
+	return "", errors.New("invalid toType")
+}
+
+func textToBin(text string) string {
 	var buf bytes.Buffer
 
-	for _, c := range str {
+	for _, c := range text {
 		v := strconv.FormatInt(int64(c), 2)
 		for i := 0; i < (8-len(v)%8)%8; i++ {
 			buf.WriteByte('0')
@@ -84,10 +182,52 @@ func strToBin(str string) string {
 	return buf.String()[:buf.Len()-1]
 }
 
-func strToHex(str string) string {
+func binToText(bin string) string {
 	var buf bytes.Buffer
 
-	for _, c := range str {
+	for _, c := range strings.Split(bin, " ") {
+		v, err := strconv.ParseInt(c, 2, 64)
+		if err != nil {
+			panic(err)
+		}
+		buf.WriteRune(rune(v))
+	}
+
+	return buf.String()
+}
+
+func byteToBin(inBytes []byte) string {
+	var buf bytes.Buffer
+
+	for _, c := range inBytes {
+		v := strconv.FormatInt(int64(c), 2)
+		for i := 0; i < (8-len(v)%8)%8; i++ {
+			buf.WriteByte('0')
+		}
+		buf.WriteString(v)
+		buf.WriteByte(' ')
+	}
+	return buf.String()[:buf.Len()-1]
+}
+
+func binToByte(bin string) []byte {
+	var buf bytes.Buffer
+
+	for _, c := range strings.Split(bin, " ") {
+		v, err := strconv.ParseInt(c, 2, 64)
+		if err != nil {
+			panic(err)
+		}
+		buf.WriteByte(byte(v))
+	}
+
+	return buf.Bytes()
+}
+
+func textToHex(text string) string {
+	var buf bytes.Buffer
+
+	for _, c := range text {
 		v := strconv.FormatInt(int64(c), 16)
 		for i := 0; i < (2-len(v)%2)%2; i++ {
 			buf.WriteByte('0')
@@ -99,10 +239,24 @@ func strToHex(str string) string {
 	return buf.String()[:buf.Len()-1]
 }
 
-func byteToHex(str []byte) string {
+func hexToText(hex string) string {
 	var buf bytes.Buffer
 
-	for _, c := range str {
+	for _, c := range strings.Split(hex, " ") {
+		v, err := strconv.ParseInt(c, 16, 64)
+		if err != nil {
+			panic(err)
+		}
+		buf.WriteRune(rune(v))
+	}
+
+	return buf.String()
+}
+
+func byteToHex(inBytes []byte) string {
+	var buf bytes.Buffer
+
+	for _, c := range inBytes {
 		v := strconv.FormatInt(int64(c), 16)
 		for i := 0; i < (2-len(v)%2)%2; i++ {
 			buf.WriteByte('0')
@@ -114,10 +268,24 @@ func byteToHex(str []byte) string {
 	return buf.String()[:buf.Len()-1]
 }
 
-func strToDec(str string) string {
+func hexToByte(hex string) []byte {
 	var buf bytes.Buffer
 
-	for _, c := range str {
+	for _, c := range strings.Split(hex, " ") {
+		v, err := strconv.ParseInt(c, 16, 64)
+		if err != nil {
+			panic(err)
+		}
+		buf.WriteByte(byte(v))
+	}
+
+	return buf.Bytes()
+}
+
+func textToDec(text string) string {
+	var buf bytes.Buffer
+
+	for _, c := range text {
 		buf.WriteString(strconv.Itoa(int(c)))
 		buf.WriteByte(' ')
 	}
@@ -125,6 +293,41 @@ func strToDec(str string) string {
 	return buf.String()[:buf.Len()-1]
 }
 
-func strToBase64(str string) string {
-	return base64.StdEncoding.EncodeToString([]byte(str))
+func decToText(dec string) string {
+	var buf bytes.Buffer
+
+	for _, c := range strings.Split(dec, " ") {
+		v, err := strconv.ParseInt(c, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		buf.WriteRune(rune(v))
+	}
+
+	return buf.String()
+}
+
+func byteToDec(inBytes []byte) string {
+	var buf bytes.Buffer
+
+	for _, c := range inBytes {
+		buf.WriteString(strconv.Itoa(int(c)))
+		buf.WriteByte(' ')
+	}
+
+	return buf.String()[:buf.Len()-1]
+}
+
+func decToByte(dec string) []byte {
+	var buf bytes.Buffer
+
+	for _, c := range strings.Split(dec, " ") {
+		v, err := strconv.ParseInt(c, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		buf.WriteByte(byte(v))
+	}
+
+	return buf.Bytes()
 }
--- internal/devtoys/text_binary_conversion_app_test.go
+++ internal/devtoys/text_binary_conversion_app_test.go
@@ -8,33 +8,25 @@ import (
 
 func TestStrToHex(t *testing.T) {
 	Convey("TestStrToHex", t, func() {
-		So(strToHex("hello"), ShouldEqual, "68 65 6c 6c 6f")
-		So(strToHex("world"), ShouldEqual, "77 6f 72 6c 64")
-		So(strToHex("你好"), ShouldEqual, "4f60 597d")
-	})
-}
-
-func TestStrToBase64(t *testing.T) {
-	Convey("TestStrToBase64", t, func() {
-		So(strToBase64("hello"), ShouldEqual, "aGVsbG8=")
-		So(strToBase64("world"), ShouldEqual, "d29ybGQ=")
-		So(strToBase64("你好"), ShouldEqual, "5L2g5aW9")
+		So(textToHex("hello"), ShouldEqual, "68 65 6c 6c 6f")
+		So(textToHex("world"), ShouldEqual, "77 6f 72 6c 64")
+		So(textToHex("你好"), ShouldEqual, "4f60 597d")
 	})
 }
 
 func TestStrToBin(t *testing.T) {
 	Convey("TestStrToBin", t, func() {
-		So(strToBin("hello"), ShouldEqual, "01101000 01100101 01101100 01101100 01101111")
-		So(strToBin("world"), ShouldEqual, "01110111 01101111 01110010 01101100 01100100")
-		So(strToBin("你好"), ShouldEqual, "0100111101100000 0101100101111101")
+		So(textToBin("hello"), ShouldEqual, "01101000 01100101 01101100 01101100 01101111")
+		So(textToBin("world"), ShouldEqual, "01110111 01101111 01110010 01101100 01100100")
+		So(textToBin("你好"), ShouldEqual, "0100111101100000 0101100101111101")
 	})
 }
 
 func TestStrToDec(t *testing.T) {
 	Convey("TestStrToDec", t, func() {
-		So(strToDec("hello"), ShouldEqual, "104 101 108 108 111")
-		So(strToDec("world"), ShouldEqual, "119 111 114 108 100")
-		So(strToDec("你好"), ShouldEqual, "20320 22909")
+		So(textToDec("hello"), ShouldEqual, "104 101 108 108 111")
+		So(textToDec("world"), ShouldEqual, "119 111 114 108 100")
+		So(textToDec("你好"), ShouldEqual, "20320 22909")
 	})
 }
 
@@ -44,101 +36,97 @@ func TestConvertTextBinaryConversion(t *testing.T) {
 	Convey("TestConvertTextBinaryConversion", t, func() {
 		Convey("bin", func() {
 			req := &TextBinaryConversionReq{
-				Text: "hello",
-				To:   "bin",
+				In:     "hello",
+				ToType: "bin",
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "01101000 01100101 01101100 01101100 01101111")
+			So(res.To, ShouldEqual, "01101000 01100101 01101100 01101100 01101111")
 		})
 
 		Convey("bin without space", func() {
 			req := &TextBinaryConversionReq{
-				Text:         "hello",
-				To:           "bin",
-				WithoutSpace: true,
+				In:     "hello",
+				ToType: "bin",
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "0110100001100101011011000110110001101111")
+			So(res.To, ShouldEqual, "0110100001100101011011000110110001101111")
 		})
 
 		Convey("bin without fill zero", func() {
 			req := &TextBinaryConversionReq{
-				Text:            "hello",
-				To:              "bin",
+				In:              "hello",
+				ToType:          "bin",
 				WithoutFillZero: true,
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "1101000 1100101 1101100 1101100 1101111")
+			So(res.To, ShouldEqual, "1101000 1100101 1101100 1101100 1101111")
 		})
 
 		Convey("bin without space and fill zero", func() {
 			req := &TextBinaryConversionReq{
-				Text:            "hello",
-				To:              "bin",
-				WithoutSpace:    true,
+				In:              "hello",
+				ToType:          "bin",
 				WithoutFillZero: true,
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "11010001100101110110011011001101111")
+			So(res.To, ShouldEqual, "11010001100101110110011011001101111")
 		})
 
 		Convey("hex", func() {
 			req := &TextBinaryConversionReq{
-				Text: "hello",
-				To:   "hex",
+				In:     "hello",
+				ToType: "hex",
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "68 65 6C 6C 6F")
+			So(res.To, ShouldEqual, "68 65 6C 6C 6F")
 		})
 
 		Convey("hex without space", func() {
 			req := &TextBinaryConversionReq{
-				Text:         "hello",
-				To:           "hex",
-				WithoutSpace: true,
+				In:     "hello",
+				ToType: "hex",
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "68656C6C6F")
+			So(res.To, ShouldEqual, "68656C6C6F")
 		})
 
 		Convey("hex without fill zero", func() {
 			req := &TextBinaryConversionReq{
-				Text:            "hello",
-				To:              "hex",
+				In:              "hello",
+				ToType:          "hex",
 				WithoutFillZero: true,
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "68 65 6C 6C 6F")
+			So(res.To, ShouldEqual, "68 65 6C 6C 6F")
 		})
 
 		Convey("hex without space and fill zero", func() {
 			req := &TextBinaryConversionReq{
-				Text:            "hello",
-				To:              "hex",
-				WithoutSpace:    true,
+				In:              "hello",
+				ToType:          "hex",
 				WithoutFillZero: true,
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "68656C6C6F")
+			So(res.To, ShouldEqual, "68656C6C6F")
 		})
 
 		Convey("hex lower case", func() {
 			req := &TextBinaryConversionReq{
-				Text:      "hello",
-				To:        "hex",
+				In:        "hello",
+				ToType:    "hex",
 				LowerCase: true,
 			}
 			res, err := app.ConvertTextBinary(req)
 			So(err, ShouldBeNil)
-			So(res.Text, ShouldEqual, "68 65 6c 6c 6f")
+			So(res.To, ShouldEqual, "68 65 6c 6c 6f")
 		})
 	})
 }
