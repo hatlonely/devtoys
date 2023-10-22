9e5142cf4939a1ac5894c710d99f770dac4cfc91
hatlonely <hatlonely@gmail.com>
2023-10-22 14:37:49 +0800

    update Button.svelte and SingleSelector.svelte

--- frontend/src/lib/components/SingleSelector.svelte
+++ frontend/src/lib/components/SingleSelector.svelte
@@ -27,23 +27,25 @@
 			<span class="material-symbols-outlined">{icon}</span>
 		{/if}
 	</span>
-	<span class="flex-auto ml-5">
+	<div class="flex-auto ml-5">
 		<dt class="font-bold">{title}</dt>
 		<dd class="text-sm opacity-50">{description}</dd>
-	</span>
+	</div>
 	<div class="flex items-center space-x-2">
-		{#each labelValues as labelValue}
-			<button
-				class="btn btn-sm text-xs {value == labelValue.value
-					? 'variant-filled-primary'
-					: 'variant-soft'} hover:variant-soft-primary"
-				on:click={() => {
-					select(labelValue.value);
-				}}
-			>
-				<span class="capitalize">{labelValue.label}</span>
-			</button>
-		{/each}
+		<div class="flex-wrap space-x-2 space-y-1">
+			{#each labelValues as labelValue}
+				<button
+					class="btn btn-sm text-xs {value == labelValue.value
+						? 'variant-filled-primary'
+						: 'variant-soft'} hover:variant-soft-primary"
+					on:click={() => {
+						select(labelValue.value);
+					}}
+				>
+					<span class="capitalize">{labelValue.label}</span>
+				</button>
+			{/each}
+		</div>
 	</div>
 </div>
 
